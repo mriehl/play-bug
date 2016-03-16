24ca5a1eca6172cd97e86d9e2fbd2de50a4d48b0
Stefan Meiwald <stefan.meiwald@immobilienscout24.de>
2016-03-16 10:04:23 +0100

    Minimal Projekt zum reproduzieren des request pending bugs in play 2.5

---
+++ app/controllers/AsyncController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import akka.actor.ActorSystem
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
+
+/**
+ * This controller creates an `Action` that demonstrates how to write
+ * simple asychronous code in a controller. It uses a timer to
+ * asynchronously delay sending a response for 1 second.
+ *
+ * @param actorSystem We need the `ActorSystem`'s `Scheduler` to
+ * run code after a delay.
+ * @param exec We need an `ExecutionContext` to execute our
+ * asynchronous code.
+ */
+@Singleton
+class AsyncController @Inject() (actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
+  def postAction = Action.async {
+    request =>
+      Future.successful(NoContent)
+  }
+}
